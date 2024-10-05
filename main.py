ab65e2f76c17d2f04eb1fac1bde2c4fdd28ab015
aimabe <aimen.aberra@gmail.com>
2024-10-05 09:12:17 -0400

    modified main, added circleshape.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -17,6 +19,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
     
 if __name__ == "__main__":
     main()
