5035a0d5bd2897a90678260eb949ef9790fee03e
aimabe <aimen.aberra@gmail.com>
2024-10-12 10:54:04 -0400

    Added player rotation speed constant. Added functionality for left and right player movement.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -13,13 +14,17 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     game = True
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while(game):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
+        
     
 if __name__ == "__main__":
     main()
