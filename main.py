02232dca4e44d683250d64b5b81ff2a3829cc9ea
aimabe <aimen.aberra@gmail.com>
2024-10-14 12:54:22 -0400

    Grouped update and draw in main

--- main.py
+++ main.py
@@ -10,19 +10,32 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+        
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     game = True
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
     while(game):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+            
+        for obj in updatable:
+            obj.update(dt)
+        
         screen.fill("black")
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
+       
         dt = clock.tick(60)/1000
         
     
