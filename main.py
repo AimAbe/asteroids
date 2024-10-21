28539cef83afb45f507a48bc3e5109e9bc4ab369
aimabe <aimen.aberra@gmail.com>
2024-10-21 14:58:34 -0400

    Added collision detection

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -13,8 +16,13 @@ def main():
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
     Player.containers = (updatable, drawable)
-        
+    Asteroid.containers = (asteroids, updatable, drawable)     
+    AsteroidField.containers = updatable  
+    asteroid_field = AsteroidField()
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -29,6 +37,11 @@ def main():
         for obj in updatable:
             obj.update(dt)
         
+        for asteroid in asteroids:
+            if asteroid.check_collision(player):
+                print("Game over!")
+                sys.exit()
+            
         screen.fill("black")
         
         for obj in drawable:
