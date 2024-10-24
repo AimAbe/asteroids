8495b8de38a5e20f99779120bc7c0136b4d94339
aimabe <aimen.aberra@gmail.com>
2024-10-24 16:58:31 -0400

    Added shooting, bullet speed, astroid splitting

--- asteroid.py
+++ asteroid.py
@@ -12,3 +12,20 @@ class Asteroid(CircleShape):
         
     def update(self, dt):
         self.position += self.velocity * dt
+        
+    def split(self,):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            random_angle = random.uniform(20,50)
+            vector_one = self.velocity.rotate(random_angle)
+            vector_two = self.velocity.rotate(-random_angle)
+            
+            radius_change = self.radius - ASTEROID_MIN_RADIUS 
+            
+            split_asteroid = Asteroid(self.position.x, self.position.y, radius_change)
+            split_asteroid.velocity = vector_one * 1.2
+            
+            split_asteroid = Asteroid(self.position.x, self.position.y, radius_change)
+            split_asteroid.velocity = vector_two * 1.2
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -17,8 +18,10 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     
     Player.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)     
     AsteroidField.containers = updatable  
     asteroid_field = AsteroidField()
@@ -41,7 +44,11 @@ def main():
             if asteroid.check_collision(player):
                 print("Game over!")
                 sys.exit()
-            
+            for shot in shots:
+                if asteroid.check_collision(shot):
+                    shot.kill()
+                    asteroid.split()
+                
         screen.fill("black")
         
         for obj in drawable:
